39fd28a09ca09102421af7bb6ba8bebce8e43cd8
Gabriel Ostolaza <Gabe.Osto@gmail.com>
2022-08-10 21:46:20 -0700

    added current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const auth = require('../../middleware/auth');
+const Profile = require("../../models/profile");
+const User = require("../../models/User");
 
 
-//@Route  GET api/profile
-//@desc   Test route
-//@access Public
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-    // res.render('../../index.html')
+//@Route  GET api/profile/me
+//@desc   Get Current Users Profile
+//@access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user',['name','avatar']);
+
+        if(!profile) {
+            return res.status(400).json({ msg: "There is no profile for this user" });
+        }
+        res.json(profile);
+    }   
+    catch {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
